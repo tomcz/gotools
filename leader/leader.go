fbbbb6eb7061047695a6db441b3f47d609c12058
Tom Czarniecki <tomcz@users.noreply.github.com>
2021-08-16 07:51:24 +1000

    typo

--- leader/leader.go
+++ leader/leader.go
@@ -4,7 +4,7 @@ import "context"
 
 // Leader election, in distributed computing, is the process of designating
 // a single process as the organizer of some task distributed among several
-// computers (nodes). Before the task has began, all network nodes are either
+// computers (nodes). Before the task has begun, all network nodes are either
 // unaware which node will serve as the "leader" (or coordinator) of the task,
 // or unable to communicate with the current coordinator. After a leader election
 // algorithm has been run, however, each node throughout the network recognizes
