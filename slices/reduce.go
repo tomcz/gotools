d68316cea6a521e73f9df208cf97c98db9fed586
Tom Czarniecki <tomcz@users.noreply.github.com>
2023-03-12 10:05:04 +1100

    slices: fix non-classical reducer signature

--- slices/reduce.go
+++ slices/reduce.go
@@ -1,18 +1,18 @@
 package slices
 
 // Reduce a slice to a single value given an initial value and a reducer function.
-func Reduce[V any, R any](values []V, acc R, reducer func(val V, acc R) R) R {
+func Reduce[V any, A any](values []V, acc A, reducer func(acc A, val V) A) A {
 	for _, val := range values {
-		acc = reducer(val, acc)
+		acc = reducer(acc, val)
 	}
 	return acc
 }
 
 // ReduceErr allows the reducer function to fail and returns the failing error.
-func ReduceErr[V any, R any](values []V, acc R, reducer func(val V, acc R) (R, error)) (R, error) {
+func ReduceErr[V any, A any](values []V, acc A, reducer func(acc A, val V) (A, error)) (A, error) {
 	var err error
 	for _, val := range values {
-		acc, err = reducer(val, acc)
+		acc, err = reducer(acc, val)
 		if err != nil {
 			return acc, err
 		}
--- slices/reduce_test.go
+++ slices/reduce_test.go
@@ -9,19 +9,19 @@ import (
 
 func TestReduce(t *testing.T) {
 	input := []int{1, 2, 3, 4}
-	reducer := func(val int, acc int) int { return val + acc }
+	reducer := func(acc int, val int) int { return val + acc }
 	assert.Equal(t, 10, Reduce(input, 0, reducer))
 }
 
 func TestReduceErr(t *testing.T) {
 	input := []int{1, 2, 3, 4}
 
-	reducer := func(val int, acc int) (int, error) { return val + acc, nil }
+	reducer := func(acc int, val int) (int, error) { return val + acc, nil }
 	actual, err := ReduceErr(input, 0, reducer)
 	assert.NilError(t, err)
 	assert.Equal(t, 10, actual)
 
-	reducerErr := func(val int, acc int) (int, error) { return -1, errors.New("test error") }
+	reducerErr := func(acc int, val int) (int, error) { return -1, errors.New("test error") }
 	actual, err = ReduceErr(input, 0, reducerErr)
 	assert.Error(t, err, "test error")
 	assert.Equal(t, -1, actual)
