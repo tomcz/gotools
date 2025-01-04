e8836a241c25abf7b4e176ff08ad26a19d7ba72e
Tom Czarniecki <tomcz@users.noreply.github.com>
2025-01-04 13:56:00 +1100

    env: ignore untagged fields

--- env/config_test.go
+++ env/config_test.go
@@ -12,19 +12,21 @@ type msTestCfg struct {
 	Age   int    `mapstructure:"test_user_age"`
 	Admin bool   `mapstructure:"test_is_admin"`
 	Port  int    `mapstructure:"port"`
+	Ratio float64
 }
 
 func TestPopulateFromEnv_mapstructure(t *testing.T) {
 	os.Setenv("test_user_name", "Homer")
 	os.Setenv("test_user_age", "42")
 	os.Setenv("test_is_admin", "true")
-	cfg := &msTestCfg{Port: 8080}
+	cfg := &msTestCfg{Port: 8080, Ratio: 1.1}
 	err := PopulateFromEnv(cfg)
 	assert.NilError(t, err)
 	assert.Equal(t, "Homer", cfg.User)
 	assert.Equal(t, 42, cfg.Age)
 	assert.Equal(t, true, cfg.Admin)
 	assert.Equal(t, 8080, cfg.Port)
+	assert.Equal(t, 1.1, cfg.Ratio)
 }
 
 type envTestCfg struct {
@@ -32,17 +34,19 @@ type envTestCfg struct {
 	Age   int    `env:"test_user_age"`
 	Admin bool   `env:"test_is_admin"`
 	Port  int    `env:"port"`
+	Ratio float64
 }
 
 func TestPopulateFromEnv_env(t *testing.T) {
 	os.Setenv("test_user_name", "Homer")
 	os.Setenv("test_user_age", "42")
 	os.Setenv("test_is_admin", "true")
-	cfg := &envTestCfg{Port: 8080}
+	cfg := &envTestCfg{Port: 8080, Ratio: 1.1}
 	err := PopulateFromEnv(cfg, "env")
 	assert.NilError(t, err)
 	assert.Equal(t, "Homer", cfg.User)
 	assert.Equal(t, 42, cfg.Age)
 	assert.Equal(t, true, cfg.Admin)
 	assert.Equal(t, 8080, cfg.Port)
+	assert.Equal(t, 1.1, cfg.Ratio)
 }
