b76907c353741b7a16ff3b7614b188018a963d7c
Tom Czarniecki <tomcz@users.noreply.github.com>
2026-04-05 21:11:29 +1000

    ensure that cleanup is called even on run panics

--- runner/runner_test.go
+++ runner/runner_test.go
@@ -3,6 +3,7 @@ package runner
 import (
 	"errors"
 	"sync"
+	"sync/atomic"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -17,7 +18,7 @@ func TestRunner(t *testing.T) {
 		lock.Lock()
 		order = append(order, "run")
 		lock.Unlock()
-		return errors.New("run error")
+		return nil
 	})
 	app.CleanupFunc(func() {
 		lock.Lock()
@@ -26,6 +27,38 @@ func TestRunner(t *testing.T) {
 	})
 	err := app.Wait()
 
-	assert.Error(t, err, "run error")
+	assert.NilError(t, err)
 	assert.DeepEqual(t, order, []string{"run", "cleanup"})
+}
+
+func TestRunner_RunError(t *testing.T) {
+	var cleanedUp atomic.Bool
+
+	app := New()
+	app.Run(func() error {
+		return errors.New("test error")
+	})
+	app.CleanupFunc(func() {
+		cleanedUp.Store(true)
+	})
+	err := app.Wait()
+
+	assert.Error(t, err, "test error")
+	assert.Assert(t, cleanedUp.Load())
+}
+
+func TestRunner_RunPanic(t *testing.T) {
+	var cleanedUp atomic.Bool
+
+	app := New()
+	app.Run(func() error {
+		panic("test panic")
+	})
+	app.CleanupFunc(func() {
+		cleanedUp.Store(true)
+	})
+	err := app.Wait()
+
+	assert.ErrorContains(t, err, "test panic")
+	assert.Assert(t, cleanedUp.Load())
 }
